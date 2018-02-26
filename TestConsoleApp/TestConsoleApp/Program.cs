6d3c37661403f5194bb092d594d6b229dc545c31
EddieKardirsson <34234941+EddieKardirsson@users.noreply.github.com>
2018-02-26 14:16:07 +0100

    Add files via upload
    Testapplication uploaded. Anders should pull it and add some properties to the user.cs class in the project and then reupload the updated version

---
+++ TestConsoleApp/TestConsoleApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Main program = new Main();
+            program.MainProgram();
+        }
+    }
+
+    public class Main
+    {
+        public void MainProgram()
+        {
+            string text = "Testing testing ";
+            int x = 1;
+            int y = 2;
+            int z = 3;
+
+            Console.WriteLine(text + x +" "+y+" "+z+"!");
+            Console.ReadLine();
+
+            //call User class here
+        }
+    }
+}
